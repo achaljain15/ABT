20855f201166626968a3fb96a24e9491c36516e8
achaljain15 <48345767+achaljain15@users.noreply.github.com>
2020-12-30 12:30:37 +0530

    String reverse and case conversion
    First Update

---
+++ src/stringClass/Split.java
@@ -0,0 +1,27 @@
+package stringClass;
+
+public class Split {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="My name is Achal";
+		String reverse= "";
+		String[] split= name.split(" ");
+		int length=split.length;
+		StringBuffer sb;
+
+		for(int i=0;i<length;i++)	
+		{
+			if((split[i]=="My")||(split[i]=="Achal"))
+			{
+			sb=new StringBuffer(split[i]);
+			System.out.println(sb.reverse());
+			}
+			else {
+				System.out.println(split[i].toUpperCase());
+			}
+
+	}
+
+}
+}
